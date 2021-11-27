0b745b61c3ec5dccef15d8eb5e2c9098b4b0f45e
uzi <usman.ibrahim24@gmail.com>
2021-11-27 11:54:02 +0100

    fb login authentication

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-# login
+# Log-in-Page
 
-A new Flutter project.
+This is a capstone project assigned to Group 4 of SideHustle Mobile app Dev. internship
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
+## GROUP-MEMBERS
+Olatunde Success SH-IT-0057169
+Ibrahim Usman SH-IT-0021068
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
-import 'dart:js';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login/login.dart';
 import 'package:login/register.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SideHustle Group 4',
     debugShowCheckedModeBanner: false,
     initialRoute: 'login',
     routes: {
       'login': (context)=>const MyLogin(),
       'register': (context)=> const MyRegister()
     },
-  ));
+  );
+  }
 }
-
